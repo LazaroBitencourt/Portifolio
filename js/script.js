4c9ef1742efc26dee3898c866984e6fff0ce332b
Lázaro Bitencourt <113481502+LazaroBitencourt@users.noreply.github.com>
2026-03-26 21:41:40 -0300

    implementa footer

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('entrar-em-contato').addEventListener('click', function() {
-        const parte1 = "\u0035\u0035\u0033\u0038";
-        const parte2 = "\u0039\u0039\u0038\u0032\u0036\u0039\u0032\u0031\u0035";
-        const numeroCompleto = parte1 + parte2;
-        window.open('https://wa.me/' + numeroCompleto + '?text=Olá%20Lázaro,%20gostaria%20de%20mais%20informações!', '_blank');
-    });
-});
